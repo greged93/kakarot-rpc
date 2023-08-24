f83df416e3d47c9722d469950bcd94c7d26fab7e
Gregory Edison <gregory.edison1993@gmail.com>
2023-08-24 13:29:39 +0200

    increase max amount of steps

--- crates/core/src/test_utils/deploy_helpers.rs
+++ crates/core/src/test_utils/deploy_helpers.rs
@@ -608,7 +608,7 @@ impl DeployedKakarot {
 
 /// Returns a `StarknetConfig` instance customized for Kakarot.
 pub fn kakarot_starknet_config() -> StarknetConfig {
-    let kakarot_steps = 2u32.pow(24);
+    let kakarot_steps = 2u32.pow(32) - 1;
     StarknetConfig {
         disable_fee: true,
         auto_mine: true,
